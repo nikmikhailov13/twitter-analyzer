8956d8c4c483a4095af27449fb6f460706aa8c35
Nikita Mykhailov <nikmikhailov13@gmail.com>
2019-09-30 17:59:19 +0300

    project skeleton

---
+++ src/main/java/com/best/druk/twitteranalyzer/TwitterAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.best.druk.twitteranalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/best/druk/twitteranalyzer/TwitterAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.best.druk.twitteranalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
